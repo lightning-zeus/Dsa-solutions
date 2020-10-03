c57b2f859fb77d9fdb7f551864465ed43780dd74
lightning-zeus <skasif232000@gmail.com>
2020-10-03 11:51:37 +0530

    Program to add and subtract complex numbers and then display the result obtained

---
+++ complex numbers.cpp
@@ -0,0 +1,73 @@
+//Zeus was here
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <fstream>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iomanip>
+#include <assert.h> //works like magic, but at cost.
+using namespace std;
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout<<x<<endl;
+#define fo(i, n) for(int i=0; i<n; i++)
+#define fk(i, k, n) for(int i=k; i<=n; i++)
+#define fn(i, n) for(int i=n-1;i>=0;i--)
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define lli long long int
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<lli, lli>	pl;
+typedef vector<int>		vi;
+typedef vector<lli>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+struct complex{
+    int real;
+    int img;
+};
+int main() {
+    struct complex comp1,comp2;
+    cout<<"Input the first complex number"<<"\n";
+    cin>>comp1.real;
+    cin>>comp1.img;
+    cout<<"Input the second complex number"<<"\n";
+    cin>>comp2.real;
+    cin>>comp2.img;
+    cout<<"After adding it becomes: "<<comp1.real+comp2.real<<"+"<<comp1.img+comp2.img<<"i"<<"\n";
+    cout<<"After subtracting it becomes: "<<comp1.real-comp2.real<<"+"<<comp1.img-comp2.img<<"i"<<"\n";
+    
+    return 0;
+}
